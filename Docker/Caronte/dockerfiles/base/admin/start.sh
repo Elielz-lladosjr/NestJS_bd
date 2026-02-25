5c5ab8f34a164d28fcc4d7312db87eaf11e72fb8
Elielz-jadosjr <elielzetasoto285@gmail.com>
2026-02-25 09:34:15 +0100

    Añadiendo el proyecto base adaptado a elielz

---
+++ Docker/Caronte/dockerfiles/base/admin/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# carga las variables de entono pasadas desde el D.Compose
+set -e
+
+source /root/admin/base/usuarios/mainUsuarios.sh
+source /root/admin/base/ssh/mainSsh.sh
+
+# source /root/admin/base/usuarios/makeUsuariosPostgress
+
+main() {
+    # gestion usuario ---> getsUser.sh
+    # gestion del sudo ---> gestSudo.sh
+    # gestion del ssh ---> gestSsh.sh
+    mkdir -p /root/logs
+    # ...
+    touch /root/logs/informe.log
+    newUser
+    if [ "$?" -eq 0 ]
+    then
+        newSSH
+    fi
+
+    # encargada de dejar este contendor vivo en BGround. 
+    # Eliminamos el tail 
+    ## tail -f /dev/null
+    ## script's que se encargar de configurar el imagen/contenedor
+}
+
+main
